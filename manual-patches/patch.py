2fe54cc2e57a2b25bda22e4475ffb6b8dbec3c80
dherrada <dylan.herrada@adafruit.com>
2022-01-25 11:54:28 -0500

    Documented patch file

--- manual-patches/patch.py
+++ manual-patches/patch.py
@@ -1,48 +1,70 @@
 from subprocess import check_output
 import os
 
-dirlist = check_output('ls /home/dherrada/adafruit/patch/repos/', shell=1).decode('utf-8').split('\n')
+
+# Saves list of currently downloaded libraries in case something is messed up halfway through
+# a patch we can redownload the libraries that it hasn't been applied to yet
+dirlist = (
+    check_output("ls /home/dherrada/adafruit/patch/repos/", shell=1)
+    .decode("utf-8")
+    .split("\n")
+)
 dirlist.pop()
-with open('repos1.txt', 'w') as F:
+with open("repos1.txt", "w") as F:
     for d in dirlist:
-        F.write(d + '\n')
+        F.write(d + "\n")
 
-branch = 'main'
+# Sets the branch to push to
+branch = "main"
 
+# Sets the commit message to use
 commit_message = "Updated docs link, updated python docs link, updated setup.py"
 
-patch = False
+# Variables to turn on and off various parts of the patch script
+patch = True
 commit = False
-push = True
+push = False
 
+# Verifies that patching was intended to be enabled
 if patch:
-    if input(f'Patching is enabled. Confirm: ').lower() not in ("y", "yes"): 
+    if input(f"Patching is enabled. Confirm: ").lower() not in ("y", "yes"):
         print("Cancelled")
         exit(1)
 else:
     print("Patching is disabled")
 
+# Verifies that committing was intended to be enabled
 if commit:
-    if input(f'Commit message: {commit_message}\nCommitting is enabled. Confirm: ').lower() not in ("y", "yes"): 
+    if input(
+        f"Commit message: {commit_message}\nCommitting is enabled. Confirm: "
+    ).lower() not in ("y", "yes"):
         print("Cancelled")
         exit(1)
 else:
     print("Committing is disabled")
 
+# Verifies that pushing was intended to be enabled
 if push:
-    if input(f'Branch: {branch}\nPushing is enabled. Confirm: ').lower() not in ("y", "yes"): 
+    if input(f"Branch: {branch}\nPushing is enabled. Confirm: ").lower() not in (
+        "y",
+        "yes",
+    ):
         print("Cancelled")
         exit(1)
-    if input(f'Are you sure?: ').lower() not in ("y", "yes"):
+    if input(f"Are you sure?: ").lower() not in ("y", "yes"):
         print("Canceled")
         exit(1)
 else:
     print("Pushing is disabled")
 
-if input(f'Patch: {patch}, Commit: {commit}, Push: {push}\nIs this all correct? ').lower() not in ("y", "yes"):
+# Confirms options of what is enabled and disabled
+if input(
+    f"Patch: {patch}, Commit: {commit}, Push: {push}\nIs this all correct? "
+).lower() not in ("y", "yes"):
     print("Cancelled")
     exit(1)
 
+# Allows you to run patch on first X number of libraries
 num = input("How many would you like to run? (leave blank for all): ")
 if len(num):
     num = int(num)
@@ -51,39 +73,50 @@ else:
 
 i = 0
 for repo in dirlist:
-    loc = '/home/dherrada/adafruit/patch/repos/{}/'.format(repo)
+    loc = "/home/dherrada/adafruit/patch/repos/{}/".format(repo)
     os.chdir(loc)
-    print('https://github.com/adafruit/{}'.format(repo))
+    print("https://github.com/adafruit/{}".format(repo))
     if patch:
-        os.system('git pull')
-        os.system(f'git checkout -b {branch}')
-        #os.system('git pull')
+        # Ensures that the local version is up to date
+        os.system("git pull")
+        os.system(f"git checkout -b {branch}")
 
-        os.system("sed -i 's/circuitpython.readthedocs.io/docs.circuitpython.org/' README.rst")
-        os.system("sed -i 's/circuitpython.readthedocs.io/docs.circuitpython.org/' docs/conf.py")
-        os.system("sed -i 's/circuitpython.readthedocs.io/docs.circuitpython.org/' docs/index.rst")
-        os.system("sed -i 's/(\"https:\\/\\/docs.python.org\\/3.4\", None)/(\"https:\\/\\/docs.python.org\\/3\", None)/' docs/conf.py")
+        # Actual patch
+        os.system(
+            "sed -i 's/circuitpython.readthedocs.io/docs.circuitpython.org/' README.rst"
+        )
+        os.system(
+            "sed -i 's/circuitpython.readthedocs.io/docs.circuitpython.org/' docs/conf.py"
+        )
+        os.system(
+            "sed -i 's/circuitpython.readthedocs.io/docs.circuitpython.org/' docs/index.rst"
+        )
+        os.system(
+            'sed -i \'s/("https:\\/\\/docs.python.org\\/3.4", None)/("https:\\/\\/docs.python.org\\/3", None)/\' docs/conf.py'
+        )
         os.system("sed -i '/Programming Language :: Python :: 3.4/d' setup.py")
         os.system("sed -i '/Programming Language :: Python :: 3.5/d' setup.py")
 
+    # Commits the patch
     if commit:
-        os.system('git add .')
+        os.system("git add .")
         os.system(f'git commit -m "{commit_message}"')
 
+    # Pushes the patch
     if push:
-        os.system('git push')
-        #os.system(f'git push -u origin {branch}')
-        os.system('cd ..')
-        #os.system('google-chrome https://github.com/adafruit/{repo}/compare/main...adafruit:manual-patch?expand=1')
-        """
-        if input('Continue? ').lower in ('n', 'no'):
-            break
-        """
-        os.system(f'rm -rf {loc}')
-        
+        # Uncomment all lines below and comment out line directly below if you're pushing to
+        # Anything other than main/default branch
+        os.system("git push")
+        # os.system(f'git push -u origin {branch}')
+        os.system("cd ..")
+        # os.system('google-chrome https://github.com/adafruit/{repo}/compare/main...adafruit:manual-patch?expand=1')
+        # if input('Continue? ').lower in ('n', 'no'):
+        #    break
+        os.system(f"rm -rf {loc}")
 
+    # Checks to make sure max number of runs hasn't been exceeded
     i += 1
     if i == num:
         break
 
-os.chdir('/home/dherrada/adafruit/patch/')
+os.chdir("/home/dherrada/adafruit/patch/")
--- manual-patches/recloner.py
+++ manual-patches/recloner.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-with open('repos1.txt', 'r') as f:
-    os.chdir('./repos/')
+with open("repos1.txt", "r") as f:
+    os.chdir("./repos/")
     for line in f:
-        os.system(f'git clone https://github.com/adafruit/{line[:-1]}.git')
+        os.system(f"git clone https://github.com/adafruit/{line[:-1]}.git")
