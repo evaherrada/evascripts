4ccbe482fc8f8a608178912b7b9ebd4e51af01f3
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-26 20:32:13 +0800

    Delete releases directory

--- releases/test.py
+++
@@ -1,8 +0,0 @@
-lines = []
-with open("repositories.txt", "r") as f:
-    for line in f:
-        lines.append(line.split('"')[1].split(" ")[0])
-
-with open("repositories.txt", "w") as f:
-    for line in lines:
-        f.write(line + "\n")
