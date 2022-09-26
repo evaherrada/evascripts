ba742f1d77be50cfc679cb60d7ef45bc049fc63b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-26 20:31:44 +0800

    Delete manual-patches directory

--- manual-patches/recloner.py
+++
@@ -1,7 +0,0 @@
-import json
-import os
-
-with open("repos1.txt", "r") as f:
-    os.chdir("./repos/")
-    for line in f:
-        os.system(f"git clone https://github.com/adafruit/{line[:-1]}.git")
